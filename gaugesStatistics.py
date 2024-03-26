560e2886080d21434f9d3dced1eaad5f46c37d3b
yixellet <yixwellet@gmail.com>
2024-03-26 22:48:54 +0400

    r

--- gaugesStatistics.py
+++ gaugesStatistics.py
@@ -1,4 +1,5 @@
 import os
+from pandas import read_csv, DataFrame
 from getGaugeData import getGaugeData
 
 gauges = {}
@@ -9,7 +10,29 @@ for file in os.listdir(directory):
 
 years = [i for i in range(1961, 2022)]
 
-volga_gauges = ['77090', '77094', '77102', '77106', '77801', '77807', '77808', '77890', '77813']
+distanses = read_csv('data/distanses/volga.csv')
+distanses['gauge'] = distanses['gauge'].astype(str)
 
+table = DataFrame(columns=["year"] + distanses['gauge'].tolist())
+print(table)
+
+for year in years:
+    arr = [None for i in range(0, len(distanses['gauge'].tolist()))]
+    print(arr)
+    table.loc[year] = arr
+
+print(table)
+"""
 for gauge, df in gauges.items():
-    print(gauge)
+    if gauge in distanses['gauge'].tolist():
+        for year in years:
+            year_str = str(year)
+            res = df[df['date'].dt.year == year]
+            max_stage = res.loc[res['stage'] == res['stage'].max()]
+            print(max_stage)
+            max_stage_date = max_stage.iloc[0]['date']
+            print(max_stage_date)
+            table.loc[year] = max_stage_date
+
+print(table)
+"""
